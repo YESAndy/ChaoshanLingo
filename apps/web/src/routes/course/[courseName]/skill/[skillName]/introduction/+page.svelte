f34db84e1c3059420a54a081bdb4ef1ddadaa3a8
kate <kate@noreply.codeberg.org>
2026-02-18 17:32:00 +0100

    added: footer

--- apps/web/src/routes/course/[courseName]/skill/[skillName]/introduction/+page.svelte
+++ apps/web/src/routes/course/[courseName]/skill/[skillName]/introduction/+page.svelte
@@ -54,3 +54,26 @@
 		margin-bottom: var(--spacing-large);
 	}
 </style>
+
+
+<Footer>
+	<Content>
+		<Columns>
+			<Column>
+			<strong>LibreLingoRelive</strong> is a fork from LibreLingoCommunity, which is a fork from
+				<strong>LibreLingo</strong> by
+				<a href="https://github.com/kantord">Dániel Kántor</a>
+				and
+				<a href="https://github.com/LibreLingo/LibreLingo#contributors"> various contributors. </a>
+			</Column>
+			<Column>
+				The source code is licensed
+				<a href="https://opensource.org/licenses/AGPL-3.0">AGPL-3.0.</a>
+				<br />
+				<a href="https://codeberg.org/LibreLingoRelive"> Source code available on Codeberg. </a>
+			</Column>
+			<Column />
+		</Columns>
+		<p></p>
+	</Content>
+</Footer>
